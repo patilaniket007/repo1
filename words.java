3f602fcd59194b2e85bf881fea5effa0ad7a8cf8
nandan <abc@gmail..com>
2024-06-25 10:57:56 +0530

    second commit done

---
+++ words.java
@@ -0,0 +1,7 @@
+class Test{
+   public static void main(String args[]){
+        String z="welcome to java class";
+        String z1[]=z.split(" ");
+        System.out.println(z1           );
+   }
+}
